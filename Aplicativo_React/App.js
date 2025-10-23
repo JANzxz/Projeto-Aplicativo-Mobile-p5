78758b5cdad9b72648f8d5d035d473cb25c685fc
JANzxz <joaovitorexz@gmail.com>
2025-10-23 20:56:17 -0300

    consegui

--- Aplicativo_React/App.js
+++ Aplicativo_React/App.js
@@ -1,11 +1,25 @@
 import { View, Text } from 'react-native'
-import LoginScreen from './src/components/LoginScreen'
+import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import LoginScreen from "./src/screens/LoginScreen"
+import HomeScreen from "./src/screens/HomeScreen"
+import DetailsScreen from './src/screens/DetailsScreen'           
+import { NavigationContainer } from '@react-navigation/native'
+
+
+const Stack = createNativeStackNavigator()
 
 const App = () => {
   return (
-    <View style={{flex: 1, alignItems: "center", justifyContent: "center"}}>
-      <LoginScreen/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login" screenOptions={{ headerTitleAlingn: 'center' }}>
+        <Stack.Screen name="Login" component={LoginScreen} options={{ title: "Tela de Login" }} />
+        <Stack.Screen name="Home" component={HomeScreen} options={{ title: "Cursos" }} />
+        <Stack.Screen name="Details" component={DetailsScreen} options={{ title: "Detalhes do Curso" }} />
+        {/* Cada Screen recebe uma navigation como prop (propiedade) */}
+
+
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
